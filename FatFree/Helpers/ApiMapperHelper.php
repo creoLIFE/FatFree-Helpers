a01a6a82c05bee70140f932f3206baf24da7aeef
Mirek Ratman <mirek.ratman@modgen.net>
2015-09-16 15:12:54 +0200

    ApiMapperHelper

---
+++ FatFree/Helpers/ApiMapperHelper.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miroslawratman
+ * Date: 13/02/15
+ * Time: 22:07
+ */
+namespace FatFree\Helpers;
+
+class ApiMapperHelper
+{
+    /**
+     * @return mixed
+     */
+    public function getModel()
+    {
+        return $this->outputModel;
+    }
+
+}
