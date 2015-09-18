fc1a7ee0d46f0b9e1254d7f97814a6a8047f7a08
Mirek Ratman <mirek.ratman@modgen.net>
2015-09-18 13:00:51 +0200

    api mapper helper update

--- FatFree/Helpers/ApiMapperHelper.php
+++ FatFree/Helpers/ApiMapperHelper.php
@@ -9,12 +9,25 @@ namespace FatFree\Helpers;
 
 class ApiMapperHelper
 {
+    /*
+     * @var mixed $apiModel - output model
+     */
+    private $apiModel;
+
     /**
      * @return mixed
      */
-    public function getModel()
+    public function getApiModel()
     {
-        return $this->outputModel;
+        return $this->apiModel;
     }
 
+    /**
+     * @param mixed $apiModel
+     */
+    public function setApiModel($apiModel)
+    {
+        $this->apiModel = $apiModel;
+    }
+    
 }
